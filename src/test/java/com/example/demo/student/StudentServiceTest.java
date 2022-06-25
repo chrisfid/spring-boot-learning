f7986eb083c007e27ea0ea97c5e52eecd65f604d
chrisfid <krzysztof.fidyka.dev@gmail.com>
2022-06-25 17:42:17 +0200

    add: Mockito unit tests pt. 2

--- src/test/java/com/example/demo/student/StudentServiceTest.java
+++ src/test/java/com/example/demo/student/StudentServiceTest.java
@@ -5,11 +5,16 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.time.LocalDate;
+import java.time.Month;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 
@@ -34,8 +39,26 @@ class StudentServiceTest {
     }
 
     @Test
-    @Disabled
-    void addNewStudent() {
+    void willThrowWhenEmailIsTaken() {
+        // given
+        String email = "jamila@gmail.com";
+        Student student = new Student(
+                "Jamila", email, LocalDate.of(2001, Month.JANUARY, 8)
+        );
+
+        // when
+        underTest.addNewStudent(student);
+
+        // then
+        ArgumentCaptor<Student> studentArgumentCaptor
+                = ArgumentCaptor.forClass(Student.class);
+
+        verify(studentRepository)
+                .save(studentArgumentCaptor.capture());
+
+        Student capturedStudent = studentArgumentCaptor.getValue();
+
+        assertThat(capturedStudent).isEqualTo(student);
     }
 
     @Test
